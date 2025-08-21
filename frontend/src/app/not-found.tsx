7160e9e831cd47ebfce49c9abe2685d129f13a97
mzaid0 <m.zaid.connect@gmail.com>
2025-08-22 04:30:49 +0500

    Frontend: Not found page

---
+++ frontend/src/app/not-found.tsx
@@ -0,0 +1,12 @@
+import { NotFoundPage } from '@/components/ui/404-page-not-found'
+import React from 'react'
+
+const NotFound = () => {
+    return (
+        <div className="w-full">
+            <NotFoundPage />
+        </div>
+    )
+}
+
+export default NotFound
